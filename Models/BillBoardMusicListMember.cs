c174f10800939ff3ada049cbceddfe2cdd927c3b
kemora08 <kevin_mora89@hotmail.com>
2020-09-27 13:44:39 -0500

    initial commit

--- Models/BillBoardMusicListMember.cs
+++ Models/BillBoardMusicListMember.cs
@@ -7,20 +7,17 @@ using System.Threading.Tasks;
 
 namespace capstone.Models
 {
-    public class BillBoardMusicList
+    public class BillBoardMusicListMember
     {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         [Key]
         public int Id { get; set; }
-        public string Name { get; set; }
         public string Artist { get; set; }
-        public string Title {get; set; }
         public string song { get; set; }
         public string duration { get; set; }
         public int Genre { get; set; }
         public bool IsActiveMember { get; set;}
         public string UserId { get; set; }
         public ApplicationUser User { get; set; }
-       
-    }
+       }
 }
